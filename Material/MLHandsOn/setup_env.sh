43bc0a744a866f84b7bcc0ab45c92dff72cd7df5
Wei Lee <whus6207@gmail.com>
2016-10-08 22:39:19 +0000

    ml setup_env

---
+++ Material/MLHandsOn/setup_env.sh
@@ -0,0 +1,5 @@
+sudo pip3 install numpy
+sudo pip3 install scikit-learn
+sudo pip3 install jupyter
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc0-cp34-cp34m-linux_x86_64.whl
+sudo pip3 install --upgrade $TF_BINARY_URL
